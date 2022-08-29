85974df46e53ed8f545cadeeb9a9ed565189ae2a
AmirHosseinAkb <amirbvk1@gmail.com>
2022-08-30 01:00:22 +0430

    ConfirmProductGroupQuery

---
+++ DigikalaQuery/Contracts/ProductGroup/ProductGroupQueryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigikalaQuery.Contracts.ProductGroup
+{
+    public class ProductGroupQueryModel
+    {
+        public long GroupId { get; set; }
+        public string GroupTitle { get; set; }
+        public long? ParentId { get; set; }
+        public string? ImageName { get; set; }
+    }
+}
---
+++ DigikalaQuery/Queries/ProductGroupQuery.cs
@@ -0,0 +1,25 @@
+﻿using DigikalaQuery.Contracts.ProductGroup;
+using ShopManagement.Infrastructure.EfCore;
+
+namespace DigikalaQuery.Queries
+{
+    public class ProductGroupQuery:IProductGroupQuery
+    {
+        private readonly ShopContext _shopContext;
+
+        public ProductGroupQuery(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+        public List<ProductGroupQueryModel> GetProductGroups()
+        {
+            return _shopContext.ProductGroups.Select(g => new ProductGroupQueryModel()
+            {
+                GroupId = g.GroupId,
+                ImageName = g.ImageName,
+                GroupTitle = g.GroupTitle,
+                ParentId = g.ParentId
+            }).ToList();
+        }
+    }
+}
