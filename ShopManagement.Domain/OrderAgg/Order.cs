a227d7e414a6821a793219b242f03f9f0cff5ca7
AmirHosseinAkb <amirbvk1@gmail.com>
2022-09-24 22:43:23 +0330

    ConfirmOrderClasses

--- ShopManagement.Domain/OrderAgg/Order.cs
+++ ShopManagement.Domain/OrderAgg/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,56 @@ namespace ShopManagement.Domain.OrderAgg
 {
     public class Order
     {
-        public long OrderId { get; set; }
-        public long? DiscountId { get; set; }
-        public long? AddressId { get; set; }
-        public long UserId { get; set; }
-        public int OrderSum { get; set; }
-        public string TrackingNumber { get; set; }
-        public byte PaymentType { get; set; } // 1 For Payment From Wallet ;2 For Payment From Bank
-        public byte Status { get; set; }// 1 For Not Paid;2 For Is Waiting;3 For Order Sent
-        public bool IsClosed { get; set; }
-        public DateTime CreationDate { get; set; }
+        public long OrderId { get;private set; }
+        public long UserId { get;private set; }
+        public long? DiscountId { get;private set; }
+        public long? AddressId { get;private set; }
+        public int OrderSum { get;private set; }
+        public string TrackingNumber { get;private set; }
+        public byte PaymentType { get;private set; } // 1 For Payment From Wallet ;2 For Payment From Bank
+        public byte Status { get;private set; }// 1 For Not Paid;2 For Is Waiting;3 For Order Sent
+        public bool IsClosed { get;private set; }
+        public DateTime CreationDate { get;private set; }
+        public bool IsDeleted { get;private set; }
+
+        public List<OrderItem> OrderItems { get; private set; }
+
+        protected Order()
+        {
+            
+        }
+
+        public Order(long userId, long? discountId, long? addressId, int orderSum, string trackingNumber, byte paymentType, byte status, bool isClosed, DateTime creationDate, bool isDeleted)
+        {
+            UserId = userId;
+            DiscountId = discountId;
+            AddressId = addressId;
+            OrderSum = orderSum;
+            TrackingNumber = trackingNumber;
+            PaymentType = paymentType;
+            Status = status;
+            IsClosed = isClosed;
+            CreationDate = creationDate;
+            IsDeleted = isDeleted;
+        }
+
+        public void Edit(long userId, long? discountId, long? addressId, int orderSum, string trackingNumber, byte paymentType, byte status, bool isClosed, DateTime creationDate, bool isDeleted)
+        {
+            UserId = userId;
+            DiscountId = discountId;
+            AddressId = addressId;
+            OrderSum = orderSum;
+            TrackingNumber = trackingNumber;
+            PaymentType = paymentType;
+            Status = status;
+            IsClosed = isClosed;
+            CreationDate = creationDate;
+            IsDeleted = isDeleted;
+        }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+        }
     }
 }
--- ShopManagement.Domain/OrderAgg/OrderItem.cs
+++ ShopManagement.Domain/OrderAgg/OrderItem.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Domain.ProductColorAgg;
 
 namespace ShopManagement.Domain.OrderAgg
 {
-    internal class OrderItem
+    public class OrderItem
     {
+        public long ItemId { get; set; }
+        public long OrderId { get; set; }
+        public long ProductId { get; set; }
+        public long ColorId { get; set; }
+        public int UnitPrice { get; set; }
+        public byte Count { get; set; }
+
+
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+        public ProductColor ProductColor { get; set; }
     }
 }
