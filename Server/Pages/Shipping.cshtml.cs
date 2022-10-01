26805112aa154ef41e819fc730b32748868369e6
AmirHosseinAkb <amirbvk1@gmail.com>
2022-10-02 02:19:10 +0330

    AddCartLayout

---
+++ Server/Pages/Shipping.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Server.Pages
+{
+    public class ShippingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
